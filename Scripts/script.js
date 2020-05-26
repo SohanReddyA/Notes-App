f3078045bf24eea545cc032cb792f6ed297d8def
Sohan Reddy <pingusohan@gmail.com>
2020-05-26 22:39:42 +0530

    First commit

---
+++ Scripts/script.js
@@ -0,0 +1,50 @@
+var i = 0,
+  j = 0;
+function Theme() {
+  if (document.getElementById('navbar').classList.contains('bg-light')) {
+    document.getElementById('navbar').classList.remove('bg-light');
+    document.getElementById('navbar').classList.remove('navbar-light');
+    document.getElementById('navbar').classList.add('bg-dark');
+    document.getElementById('navbar').classList.add('navbar-dark');
+    document.getElementById('add').classList.remove('btn-outline-secondary');
+    document.getElementById('add').classList.add('btn-light');
+    document.getElementById('logo').classList.remove('fas');
+    document.getElementById('logo').classList.add('far');
+    document.getElementById('dark').innerHTML = 'Change Theme to Light';
+    document.body.style.backgroundColor = 'grey';
+  } else {
+    document.getElementById('navbar').classList.remove('bg-dark');
+    document.getElementById('navbar').classList.remove('navbar-dark');
+    document.getElementById('navbar').classList.add('bg-light');
+    document.getElementById('navbar').classList.add('navbar-light');
+    document.getElementById('add').classList.remove('btn-light');
+    document.getElementById('add').classList.add('btn-outline-secondary');
+    document.getElementById('logo').classList.remove('far');
+    document.getElementById('logo').classList.add('fas');
+    document.getElementById('dark').innerHTML = 'Change Theme to Dark';
+    document.body.style.backgroundColor = 'white';
+  }
+}
+function clone() {
+  const div = document.createElement('div');
+  div.innerHTML = template();
+  document.getElementById('NoteContainer').after(div);
+}
+function template() {
+  return `
+  <div class="Note">
+  <div class="NoteHeader">
+    <span class="NoteClose">
+      <a href="#" onclick='this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false;'>
+        <i class="far fa-window-close"></i>
+      </a>
+    </span>
+  </div>
+  <div class="NoteTitle" >
+    <input type="text" id="Title" class="inp" placeholder="Enter the Title"/>
+  </div>
+  <div class="NoteBody">
+    <input type="text" id="Body" class="inp" placeholder="Enter the body"/>
+  </div>
+</div>`;
+}
